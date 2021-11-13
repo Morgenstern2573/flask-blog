0848b321e507da1dd60a68b31c63e64d27f99671
Bosun Akinyemi <azurnwa.p@gmail.com>
2021-11-13 20:15:40 +0100

    Refactor code to use production db

--- blogserver/db.py
+++ blogserver/db.py
@@ -1,10 +1,14 @@
 import psycopg2
 import psycopg2.extras
+import os
 
 
 def get_db():
-    conn = psycopg2.connect(
-        "host=localhost port=5432 dbname=blog_cms user=postgres password=maitnriot")
+    DATABASE_URL = os.environ['DATABASE_URL']
+    if os.environ.get("FLASK_ENV") != "development":
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    else:
+        conn = psycopg2.connect(DATABASE_URL)
     return conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 
